32c93eda8bf15b544a2fd8fd59ab0719c10f6485
vrunoa <vruno.alassia@gmail.com>
2015-08-08 20:23:44 -0300

    add electron base app

---
+++ electron.js
@@ -0,0 +1,27 @@
+var app = require('app');
+var BrowserWindow = require('browser-window');
+var mainWindow = null;
+
+function quit(err) {
+  if(err) console.log(err);
+  mainWindow = null;
+  app.quit();
+}
+
+app.on('window-all-closed', function() {
+  if (process.platform != 'darwin')
+    app.quit();
+});
+
+app.on('ready', function() {
+  
+  var options = {
+    width: 1040, 
+    height: 700, 
+    'min-width':600, 
+    'min-height':670
+  }
+  mainWindow = new BrowserWindow(options);
+  mainWindow.loadUrl('http://trello.com');
+  //mainWindow.openDevTools();
+});
